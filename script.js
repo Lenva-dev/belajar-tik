c15149b8d7f59671bfcc8fc9412e82ba45b75730
Lenva-dev <fullsethua@gmail.com>
2025-10-23 10:12:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-const tombolSapa = document.getElementById(
+//pilih tombol berdasarkan id nya//
+const tombolSapa = document.getElementById('tombol');
+//tambahkan event listener untuk aksi click//
+tombolSapa.addEventListener('click', function() {
+  //jalankan kode ini jika tombol di klik//
+  alert('suwon yo lekk');
+});
